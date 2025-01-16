baa7f78f4936ff9d635f4bc6b6453ccc207db1b8
Rados Nikolic <radosnikolic@Radoss-MBP.netis.cc>
2025-01-16 18:31:45 +0100

    Install Laravel breeze, configure routes, configure tailwind colors

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'John Doe',
+            'email' => 'johndoe@example.com',
+            'password' => Hash::make('password123'),
+        ]);
+
+        User::create([
+            'name' => 'Jane Doe',
+            'email' => 'janedoe@example.com',
+            'password' => Hash::make('password456'),
+        ]);
+    }
+}
